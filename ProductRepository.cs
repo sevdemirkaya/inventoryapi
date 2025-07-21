599dfab1d0bdb94e209ed616efcbde5c4d7883ff
sevdemirkaya <sevdemirkaya@icloud.com>
2025-07-21 10:13:03 +0300

    ProductController CRUD tamamlandı

---
+++ ProductRepository.cs
@@ -0,0 +1,33 @@
+using InventoryApi.Model.Entities;
+using InventoryApi.Model.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApi.DataAccess.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly AppDbContext _context;
+    public ProductRepository(AppDbContext context) => _context = context;
+
+    public async Task<List<Product>> GetAllAsync() => await _context.Products.ToListAsync();
+    public async Task<Product?> GetByIdAsync(int id) => await _context.Products.FindAsync(id);
+    public async Task AddAsync(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Product product)
+    {
+        _context.Products.Update(product);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product is null) return;
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+    }
+}
--- obj/Debug/net8.0/InventoryApi.DataAccess.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventoryApi.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryApi.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+227877e940e062581394737f776e7d72dc638d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryApi.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryApi.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
