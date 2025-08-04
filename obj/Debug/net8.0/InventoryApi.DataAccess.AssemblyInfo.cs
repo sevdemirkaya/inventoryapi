4357284ed64a47fc70756e113f2d4dcac908a70e
sevdemirkaya <sevdemirkaya@icloud.com>
2025-08-04 13:08:07 +0300

    son yapı

--- obj/Debug/net8.0/InventoryApi.DataAccess.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventoryApi.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryApi.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb39f62afd0a4691fe3ec6164a6cad7699740643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2261932ccb34fbff53871d0d4f59a52c7d437133")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryApi.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryApi.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
